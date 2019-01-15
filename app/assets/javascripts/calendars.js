200f666883a1927707fdc2e203f92e21eb5fddf8
pgrn <pgorni@teknik.io>
2019-01-15 03:43:33 +0100

    Fixed popover behavior while using Turbolinks

--- app/assets/javascripts/calendars.js
+++ app/assets/javascripts/calendars.js
@@ -1,10 +1,19 @@
-$(function () {
+$(
+  loadPopovers()
+)
 
+$(document).on('turbolinks:load', function() {
+  loadPopovers();
+})
+
+function loadPopovers() {
   for (let e of $('[data-toggle="popover"]')) {
     $(e).popover({
       html: true,
       content: e.querySelector('.popup-content')
     })
   }
+}
 
-})
+
+
